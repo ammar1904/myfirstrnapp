2b07d76c3aae539181dfd1490c9ae288bc23f131
ammar1904 <ammarkhan1904@gmail.com>
2022-12-29 19:08:00 +0500

    Commited 6

--- AwesomeProject/app/practice/Components/CustomAppPicker.js
+++ AwesomeProject/app/practice/Components/CustomAppPicker.js
@@ -1,16 +1,26 @@
-import { StyleSheet, TextInput, View } from 'react-native'
+import { Button, Modal, StyleSheet, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 import colors from '../../config/colors'
 import CustomText from './CustomText'
 
 const CustomAppPicker = ({ name, color, placeholder, size, ...otherProps }) => {
+
+    const [modalVisible, setModalVisible] = useState(false)
+
   return (
+    <>
+    <TouchableOpacity onPress={ () => setModalVisible(true) }>
     <View style={styles.container}>
         <MaterialCommunityIcons color={color} size={size} name={name} style={styles.icon} placeholder={placeholder}/>
         <CustomText style={styles.text}>{placeholder}</CustomText>
         <MaterialCommunityIcons name="chevron-down" color="black" size={30} style={styles.chevron} />
     </View>
+    </TouchableOpacity>
+    <Modal visible={modalVisible} animationType="slide">
+        <Button title="Close" onPress={() => setModalVisible(false)} />
+    </Modal>
+    </>
   )
 }
 
